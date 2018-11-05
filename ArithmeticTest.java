153d0268011c5ec96d63ccf176110eca25c371d7
lhc <l1214095974@yeah.net>
2018-11-05 15:23:20 +0800

    calculate

---
+++ ArithmeticTest.java
@@ -0,0 +1,12 @@
+package visibleArithmeticExercise;
+
+import java.io.IOException;
+
+public class ArithmeticTest {
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        MyExGUI exercise = new MyExGUI();
+    }
+
+}
+
